e6cc53d7e7419531f2b9ee4f0190d0422b073769
Tom Levin <tlevin8@gmail.com>
2020-09-03 14:06:03 +0200

    Updates

---
+++ precache-manifest.40d4e384ff85c0314add96e6d0575b58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076562ccf15a470d88aadbe602ff5cf5",
+    "url": "/puppet-pals/index.html"
+  },
+  {
+    "revision": "ac354f9f94316a584c4b",
+    "url": "/puppet-pals/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fb43cf6f38fb7f730d57",
+    "url": "/puppet-pals/static/css/main.42356629.chunk.css"
+  },
+  {
+    "revision": "ac354f9f94316a584c4b",
+    "url": "/puppet-pals/static/js/2.86d734ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/puppet-pals/static/js/2.86d734ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb43cf6f38fb7f730d57",
+    "url": "/puppet-pals/static/js/main.3eb9041b.chunk.js"
+  },
+  {
+    "revision": "727efcf0aca759d90e44",
+    "url": "/puppet-pals/static/js/runtime-main.9ec2f379.js"
+  },
+  {
+    "revision": "fd9d6157b57c44a42a141072d7710bd7",
+    "url": "/puppet-pals/static/media/FunCartoon2.fd9d6157.woff"
+  }
+]);
